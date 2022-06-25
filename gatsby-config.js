140c173839ee71d0b1e350d660d61c0b0b4ed04f
sc13sees <sc13sees@gmail.com>
2022-06-25 10:51:35 +0800

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: '/', // Change to `/running_page` when running on github pages
   siteMetadata: {
-    siteTitle: 'Running Page',
+    siteTitle: 'Gordon的跑步纪录',
     siteUrl: 'https://yihong.run',
-    logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+    logo: 'https://s1.ax1x.com/2022/06/23/jCmO74.png',
     description: 'Personal site and blog',
     navLinks: [
       {
